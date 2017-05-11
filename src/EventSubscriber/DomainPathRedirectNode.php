850e3ba4435b4a4bb76c84147ac26cd2d5ed3c3a
Andrei Cherednichenko <acherednichenko@adyax.com>
2017-05-11 12:15:04 +0300

    redirect node for custom domain alias

---
+++ src/EventSubscriber/DomainPathRedirectNode.php
@@ -0,0 +1,195 @@
+<?php
+
+/**
+* @file
+* Contains \Drupal\domain_path\EventSubscriber\DomainPathRedirectNode
+*/
+
+namespace Drupal\domain_path\EventSubscriber;
+
+use Drupal\Core\Url;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\redirect\RedirectChecker;
+use Symfony\Component\Routing\RequestContext;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Path\AliasManager;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\PathProcessor\InboundPathProcessorInterface;
+use Drupal\redirect\Exception\RedirectLoopException;
+use Drupal\domain_path\DomainPathRepository;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Drupal\domain\DomainNegotiatorInterface;
+
+class DomainPathRedirectNode implements EventSubscriberInterface {
+
+
+  protected $domain_negotiator;
+
+  /** @var  \Drupal\redirect\RedirectRepository */
+  protected $redirectRepository;
+
+  /**
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * @var \Drupal\Core\Path\AliasManager
+   */
+  protected $aliasManager;
+
+  /**
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * @var \Drupal\redirect\RedirectChecker
+   */
+  protected $checker;
+
+  /**
+   * @var \Symfony\Component\Routing\RequestContext
+   */
+  protected $context;
+
+  /**
+   * A path processor manager for resolving the system path.
+   *
+   * @var \Drupal\Core\PathProcessor\InboundPathProcessorInterface
+   */
+  protected $pathProcessor;
+
+  /**
+   * Constructs a \Drupal\redirect\EventSubscriber\RedirectRequestSubscriber object.
+   *
+   * @param \Drupal\redirect\RedirectRepository $redirect_repository
+   *   The redirect entity repository.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config
+   *   The config.
+   * @param \Drupal\Core\Path\AliasManager $alias_manager
+   *   The alias manager service.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler service.
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   *   The entity manager service.
+   * @param \Drupal\redirect\RedirectChecker $checker
+   *   The redirect checker service.
+   * @param \Symfony\Component\Routing\RequestContext
+   *   Request context.
+   */
+  public function __construct(DomainNegotiatorInterface $domain_negotiator, DomainPathRepository $repository, LanguageManagerInterface $language_manager, ConfigFactoryInterface $config, AliasManager $alias_manager, ModuleHandlerInterface $module_handler, EntityManagerInterface $entity_manager, RedirectChecker $checker, RequestContext $context, InboundPathProcessorInterface $path_processor) {
+    $this->domain_negotiator = $domain_negotiator;
+    $this->redirectRepository = $repository;
+    $this->languageManager = $language_manager;
+    $this->config = $config->get('redirect.settings');
+    $this->aliasManager = $alias_manager;
+    $this->moduleHandler = $module_handler;
+    $this->entityManager = $entity_manager;
+    $this->checker = $checker;
+    $this->context = $context;
+    $this->pathProcessor = $path_processor;
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  public static function getSubscribedEvents() {
+    // This announces which events you want to subscribe to.
+    // We only need the request event for this example.  Pass
+    // this an array of method names
+    return([
+      KernelEvents::REQUEST => [
+        ['redirectNode'],
+      ]
+    ]);
+  }
+
+  /**
+  * Redirect requests for my_content_type node detail pages to node/123.
+  *
+  * @param GetResponseEvent $event
+  * @return void
+  */
+  public function redirectNode(GetResponseEvent $event) {
+    $request = clone $event->getRequest();
+
+    if (!$this->checker->canRedirect($request)) {
+      return;
+    }
+
+    // This is necessary because this also gets called on
+    // node sub-tabs such as "edit", "revisions", etc.  This
+    // prevents those pages from redirected.
+    if ($request->attributes->get('_route') !== 'entity.node.canonical') {
+      return;
+    }
+
+    // Get URL info and process it to be used for hash generation.
+    parse_str($request->getQueryString(), $request_query);
+
+    // Do the inbound processing so that for example language prefixes are
+    // removed.
+    $path = $this->pathProcessor->processInbound($request->getPathInfo(), $request);
+    $path = ltrim($path, '/');
+
+    $this->context->fromRequest($request);
+
+    try {
+      $node = \Drupal::routeMatch()->getParameter('node');
+      $domain = $this->domain_negotiator->getActiveDomain();
+
+      // TODO: enable for all entities types
+      $redirect = $this->redirectRepository->findMatchingRedirect($domain->id(), $node->id(), $this->languageManager->getCurrentLanguage()->getId());
+
+    }
+    catch (RedirectLoopException $e) {
+      \Drupal::logger('redirect')->warning($e->getMessage());
+      $response = new Response();
+      $response->setStatusCode(503);
+      $response->setContent('Service unavailable');
+      $event->setResponse($response);
+      return;
+    }
+
+    if (!empty($redirect)) {
+
+      // Handle internal path.
+      $url = $redirect->getUrl();
+
+      $response = new RedirectResponse($url['value'], 301);
+      $event->setResponse($response);
+
+
+      /*if ($this->config->get('passthrough_querystring')) {
+        $url->setOption('query', (array) $url->getOption('query') + $request_query);
+      }*/
+      $headers = [
+        'X-Redirect-ID' => $redirect->id(),
+      ];
+      //$response = new TrustedRedirectResponse($url->setAbsolute()->toString(), $redirect->getStatusCode(), $headers);
+      //$response->addCacheableDependency($redirect);
+      //$event->setResponse($response);
+    }
+  }
+
+}
---
+++ src/Exception/DomainPathRedirectLoopException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\domain_path\Exception;
+
+use Drupal\Component\Utility\SafeMarkup;
+use Drupal\Component\Render\FormattableMarkup;
+
+/**
+ * Exception for when a redirect loop is detected.
+ */
+class DomainPathRedirectLoopException extends \RuntimeException {
+
+  /**
+   * Formats a redirect loop exception message.
+   *
+   * @param string $path
+   *   The path that results in a redirect loop.
+   * @param int $rid
+   *   The redirect ID that is involved in a loop.
+   */
+  public function __construct($domain_id, $entity_type, $id) {
+    parent::__construct(FormattableMarkup::placeholderFormat('Redirect loop identified at %entity_type for redirect %id on domain %domain', ['%entity_type' => $path, '%id' => $id, '%domain' => $domain_id]));
+  }
+
+}
