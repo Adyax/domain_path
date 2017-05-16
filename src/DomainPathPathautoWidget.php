4e0ac1f485aed2c43a89cc6b9e9815a23ccac434
Sergey Krasulevskiy <skrasulevskiy@adyax.com>
2017-05-16 17:28:56 +0300

    task #231181: add domains selection to url path settings

--- src/DomainPathPathautoWidget.php
+++ src/DomainPathPathautoWidget.php
@@ -10,6 +10,8 @@ use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\domain\DomainLoaderInterface;
 use Drupal\Core\Path\AliasManagerInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\pathauto\PathautoGeneratorInterface;
+use Drupal\domain_path\DomainPathLoaderInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -34,6 +36,16 @@ class DomainPathPathautoWidget extends PathautoWidget implements ContainerFactor
   protected $accountManager;
 
   /**
+   * @var \Drupal\pathauto\PathautoGeneratorInterface
+   */
+  protected $pathautoGeneratorManager;
+
+  /**
+   * @var \Drupal\domain_path\DomainPathLoaderInterface
+   */
+  protected $domainPathLoaderManager;
+
+  /**
    * DomainPathPathautoWidget constructor.
    *
    * @param string $plugin_id
@@ -44,12 +56,16 @@ class DomainPathPathautoWidget extends PathautoWidget implements ContainerFactor
    * @param \Drupal\domain\DomainLoaderInterface $domain_loader_manager
    * @param \Drupal\Core\Path\AliasManagerInterface $alias_manager
    * @param \Drupal\Core\Session\AccountInterface $account_manager
+   * @param \Drupal\pathauto\PathautoGeneratorInterface $pathauto_generator_manager
+   * @param \Drupal\domain_path\DomainPathLoaderInterface $domain_path_loader_manager
    */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, DomainLoaderInterface $domain_loader_manager, AliasManagerInterface $alias_manager, AccountInterface $account_manager) {
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, DomainLoaderInterface $domain_loader_manager, AliasManagerInterface $alias_manager, AccountInterface $account_manager, PathautoGeneratorInterface $pathauto_generator_manager, DomainPathLoaderInterface $domain_path_loader_manager) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
     $this->domainLoaderManager = $domain_loader_manager;
     $this->aliasManager = $alias_manager;
     $this->accountManager = $account_manager;
+    $this->pathautoGeneratorManager = $pathauto_generator_manager;
+    $this->domainPathLoaderManager = $domain_path_loader_manager;
   }
 
   /**
@@ -64,7 +80,9 @@ class DomainPathPathautoWidget extends PathautoWidget implements ContainerFactor
       $configuration['third_party_settings'],
       $container->get('domain.loader'),
       $container->get('path.alias_manager'),
-      $container->get('current_user')
+      $container->get('current_user'),
+      $container->get('pathauto.generator'),
+      $container->get('domain_path.loader')
     );
   }
 
@@ -75,64 +93,109 @@ class DomainPathPathautoWidget extends PathautoWidget implements ContainerFactor
     $element = parent::formElement($items, $delta, $element, $form, $form_state);
     $entity = $items->getEntity();
 
-    $pattern = \Drupal::service('pathauto.generator')->getPatternByEntity($entity);
+    $pattern = $this->pathautoGeneratorManager->getPatternByEntity($entity);
     if (empty($pattern)) {
-      return $element;
+      //return $element;
     }
 
     if ($domains = $this->domainLoaderManager->loadMultipleSorted()) {
-      $current = t('<none>');
+      $entity_type = $entity->getEntityTypeId();
+      $entity_id = $entity->id();
+      $langcode = $entity->get('langcode')->value;
+      //$current = $entity_type && $entity_id ? $this->aliasManager->getAliasByPath("/$entity_type/$entity_id") : t('<none>');
+      $show_delete = FALSE;
+      $domain_path_loader = $this->domainPathLoaderManager;
 
-      $entity_id = NULL;
-      //$node = $form_state->getFormObject()->getEntity();
-      $langcode = NULL;
-      if ($entity && $entity_id = $entity->id()) {
-        $entity_type = $entity->getEntityTypeId();
-        $current = $this->aliasManager->getAliasByPath("/$entity_type/$entity_id");
-        $langcode = $entity->get('langcode')->value;
-        $user = $this->accountManager;
+      $element['domain_path'] = [
+        '#tree' => TRUE,
+        '#type' => 'details',
+        '#title' => $this->t('Domain-specific paths'),
+        '#group' => 'path_settings',
+        '#weight' => 110,
+        '#open' => TRUE,
+        '#access' => $this->accountManager->hasPermission('edit domain path entity'),
+      ];
 
-        $show_delete = FALSE;
-        $domain_path_loader = \Drupal::service('domain_path.loader');
-
-        foreach ($domains as $domain_id => $domain) {
-          $path = FALSE;
-          $properties = [
-            'entity_id' => $entity_id,
-            'language' => $langcode,
-            'domain_id' => $domain_id,
-            'entity_type' => $entity_type,
-          ];
-          if ($entity_id && $domain_paths = $domain_path_loader->loadByProperties($properties)) {
-            foreach ($domain_paths as $domain_path) {
-              $path = $domain_path->get('alias')->value;
-            }
+      foreach ($domains as $domain_id => $domain) {
+        $path = FALSE;
+        $properties = [
+          'entity_id' => $entity_id,
+          'language' => $langcode,
+          'domain_id' => $domain_id,
+          'entity_type' => $entity_type,
+        ];
+        if ($entity_id && $domain_paths = $domain_path_loader->loadByProperties($properties)) {
+          foreach ($domain_paths as $domain_path) {
+            $path = $domain_path->get('alias')->value;
           }
-
-          $default = '';
-          if ($path) {
-            $show_delete = TRUE;
-          }
-
-          $element[$domain_id] = [
-            '#type' => 'textfield',
-            '#title' => Html::escape($domain->getPath()),
-            '#default_value' => $path ? $path : $default,
-            '#access' => $user->hasPermission('edit domain path entity'),
-          ];
         }
+
+        $default = '';
+        if ($path) {
+          $show_delete = TRUE;
+        }
+
+        $element['domain_path']['domain_path_delete'] = [
+          '#type' => 'checkbox',
+          '#title' => $this->t('Delete domain-specific aliases'),
+          '#default_value' => FALSE,
+          '#access' => $show_delete,
+        ];
+
+        $element['domain_path'][$domain_id] = [
+          '#type' => 'textfield',
+          '#title' => Html::escape(rtrim($domain->getPath(), '/')),
+          '#default_value' => $path ? $path : $default,
+          '#access' => $this->accountManager->hasPermission('edit domain path entity'),
+        ];
       }
     }
 
-    /*$element['pathauto'] = array(
-      '#type' => 'checkbox',
-      '#title' => $this->t('Generate automatic URL alias'),
-      '#default_value' => $entity->path->pathauto,
-      '#description' => $description,
-      '#weight' => -1,
-    );   */
-
     return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function validateFormElement(array &$element, FormStateInterface $form_state) {
+    //TODO: refactor this method later
+    parent::validateFormElement($element, $form_state);
+
+    $entity = $form_state->getFormObject()->getEntity();
+    $entity_id = $entity->id();
+    $path_values = $form_state->getValue('path');
+    $domain_path_values = isset($path_values[0]['domain_path']) ? $path_values[0]['domain_path'] : [];
+    if (!empty($domain_path_values['domain_path_delete'])) {
+      return;
+    }
+    unset($domain_path_values['domain_path_delete']);
+    $alias = isset($path_values[0]['alias']) ? $path_values[0]['alias'] : NULL;
+    $domain_path_loader = \Drupal::service('domain_path.loader');
+    $domains = \Drupal::service('domain.loader')->loadMultipleSorted();
+    // Make sure we don't duplicate anything.
+    foreach ($domain_path_values as $domain_id => $path) {
+      $key = ($domain_id == -1) ? 0 : $domain_id;
+      if (!empty($path) && $path == $alias) {
+        $form_state->setError($element['domain_path'][$key], t('Domain path "%path" matches the default path alias. You may leave the element blank.', ['%path' => $path]));
+      }
+      elseif (!empty($path)) {
+        $path_value = rtrim(trim($path), " \\/");
+        if ($path_value && $path_value[0] !== '/') {
+          $form_state->setError($element['domain_path'][$key], t('Domain path "%path" needs to start with a slash.', ['%path' => $path]));
+        }
+        if (!empty($entity_id) && $domain_path_entity_data = $domain_path_loader->loadByProperties(['alias' => $path])) {
+          foreach ($domain_path_entity_data as $domain_path_entity) {
+            $check_entity_id = $domain_path_entity->get('entity_id')->target_id;
+            $check_domain_id = $domain_path_entity->get('domain_id')->target_id;
+            if ($check_entity_id != $entity_id
+              && $check_domain_id == $key) {
+              $domain_path = $domains[$domain_id]->getPath();
+              $form_state->setError($element['domain_path'][$key], t('Domain path %path matches an existing domain path alias for %domain_path.', ['%path' => $path, '%domain_path' => $domain_path]));
+            }
+          }
+        }
+      }
+    }
   }
 
 }
--- src/Form/DomainPathSettingsForm.php
+++ src/Form/DomainPathSettingsForm.php
@@ -2,15 +2,42 @@
 
 namespace Drupal\domain_path\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class DomainPathSettingsForm.
  * @package Drupal\domain_path\Form
  * @ingroup domain_path
  */
-class DomainPathSettingsForm extends FormBase {
+class DomainPathSettingsForm extends ConfigFormBase {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($config_factory);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * Returns a unique string identifying the form.
@@ -23,32 +50,50 @@ class DomainPathSettingsForm extends FormBase {
   }
 
   /**
-   * Form submission handler.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the form.
-   * @param FormStateInterface $form_state
-   *   An associative array containing the current state of the form.
+   * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Empty implementation of the abstract submit class.
+  protected function getEditableConfigNames() {
+    return ['domain_path.settings'];
   }
 
-
   /**
-   * Define the form used for DomainPath settings.
-   * @return array
-   *   Form definition array.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the form.
-   * @param FormStateInterface $form_state
-   *   An associative array containing the current state of the form.
+   * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['domain_path_settings']['#markup'] = $this->t('Settings form for DomainPath. Manage field settings here.');
+    $config = $this->config('domain_path.settings');
+    $enabled_entity_types = $config->get('entity_types');
 
-    return $form;
+    $form['entity_types'] = [
+      '#type' => 'details',
+      '#open' => TRUE,
+      '#title' => $this->t('Enabled entity types for domain paths'),
+      '#tree' => TRUE,
+    ];
+
+    // Get all applicable entity types.
+    foreach ($this->entityTypeManager->getDefinitions() as $entity_type_id => $entity_type) {
+      if (is_subclass_of($entity_type->getClass(), FieldableEntityInterface::class) && $entity_type->hasLinkTemplate('canonical')) {
+        $default_value = !empty($enabled_entity_types[$entity_type_id]) ? $enabled_entity_types[$entity_type_id] : NULL;
+        $form['entity_types'][$entity_type_id] = [
+          '#type' => 'checkbox',
+          '#title' => $entity_type->getLabel(),
+          '#default_value' => $default_value,
+        ];
+      }
+    }
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('domain_path.settings')
+      ->set('entity_types', $form_state->getValue('entity_types'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
   }
 
 }
